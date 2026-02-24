65236cc6b7ef3e36be0bf2e0bfff51de4bfb3af4
Varun09 <varun@zapwms.com>
2026-02-24 12:35:06 +0530

    testing 2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 app.get("/", (req, res) => {
-  res.send("hello express");
+  res.send("hello express 1");
 });
 
 app.listen(PORT, () => {
